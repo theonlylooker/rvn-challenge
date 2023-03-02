cbc4ff3dcf19994dc59db13e77a819fdd3557e8a
basurco eduardo <angelsoulsword@gmail.com>
2023-03-01 20:51:40 -0500

    fix: changing test colors for the right ones

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,22 +5,22 @@ import { faBars, faBorderAll } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar = () => {
   return (
-    <div className="bg-rvnComponentBG h-full max-w-[232px] rounded-xl pt-3">
-      <div className="flex justify-center pb-4">
+    <div className="bg-rvnComponentBG h-full min-w-[232px] rounded-xl pt-3">
+      <div className="flex justify-center pb-11">
         <img src={Logo} alt="logo of ravn" />
       </div>
-      <div className="text-gray-500 flex flex-col">
+      <div className="text-rvnTextSecondary flex flex-col">
         <a
           href="#"
           className="hover:text-rvnRed hover:bg-gradient-to-r from-rvnComponentBG  to-rvnRedGradient  p-4 focus:border-r-4 focus:border-rvnRed focus:text-rvnRed focus:bg-gradient-to-r"
         >
-          <FontAwesomeIcon icon={faBorderAll} /> DASHBOARD
+          <FontAwesomeIcon icon={faBorderAll} className="mr-3" /> DASHBOARD
         </a>
         <a
           href="#"
           className="hover:text-rvnRed hover:bg-gradient-to-r from-rvnComponentBG to-rvnRedGradient  p-4 focus:border-r-4 focus:border-rvnRed focus:text-rvnRed focus:bg-gradient-to-r"
         >
-          <FontAwesomeIcon icon={faBars} /> MY TASK
+          <FontAwesomeIcon icon={faBars} className="mr-3" /> MY TASK
         </a>
       </div>
     </div>
